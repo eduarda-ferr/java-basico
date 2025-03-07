ac434a687a0286c94006ed53071646e08e5aaeb7
Maria Eduarda <mariaeduardafb00@gmail.com>
2025-03-07 14:26:17 -0300

    adição de armazenamentos de dados das variáveis.

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal{
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número da Conta: ");
+        int numeroCliente = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Insira o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Saldo da Conta:");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "OlA " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroCliente + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+        scanner.close();
     }
 }
