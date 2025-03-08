5a1a80789cc1f9c91ea99dbba940b390d2998c78
Maria Eduarda <mariaeduardafb00@gmail.com>
2025-03-08 14:45:25 -0300

    Concatenação

---
+++ anatomia-classes/src/concat.java
@@ -0,0 +1,17 @@
+public class concat {
+    public static void main(String[] args){
+    
+        String primeiroNome = "Maria";
+        String segundoNome = "Eduarda";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome , String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+    
+}
+
